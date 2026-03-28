3d9ddf6934dad5136747ba7e644a8b20d84322db
Karnav Kmaleshkumar Prajapati <134150848+Karnav018@users.noreply.github.com>
2026-03-28 23:16:26 +0530

    exception added

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import sys
 from src.end_to_end.logger import logging
+from src.end_to_end.exception import EndToEndException
+
+
 
 if __name__ == "__main__":
     logging.info("Starting the application........")
+    
+    try: 
+        a=1/0
+    except Exception as e:
+        logging.info("Exception occurred........")
+        raise EndToEndException(e, sys) from e
---
+++ src/end_to_end/exception.py
@@ -0,0 +1,19 @@
+import sys
+from src.end_to_end import logger
+
+
+def error_message_details(error_message, error_details: sys):
+    _, _, exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] with error message: [{error_message}]"
+    return error_message
+
+
+class EndToEndException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super.__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details)
+
+    def __str__(self):
+        return self.error_message
